d2b86cea130b268d0ea181edd350f50bb9da2b7e
farhadamrah <emferhad17631@sabah.edu.az>
2021-04-19 19:09:00 +0400

    version 1

--- src/Components/Main/MainPages/ServiceOrders/ServiceOrdersData.js
+++ src/Components/Main/MainPages/ServiceOrders/ServiceOrdersData.js
@@ -1,8 +1,8 @@
 const TenderData = [
   {
-    title: 'MG Consulting MMC',
+    title: 'Buta Grup',
     // image: '',
-    header: 'MG Consulting MMC Veb saytın yaradılması üçün tender elan edir',
+    header: 'Buta Grup Veb saytın yaradılması üçün tender elan edir',
     feedbacks: {
       green: 0,
       red: 0,
@@ -11,6 +11,20 @@ const TenderData = [
     price: 0,
     addedDate: 9,
     deadLine: '13.04.2021',
+    order: 0,
+  },
+  {
+    title: 'Buta Grup',
+    // image: '',
+    header: 'Buta Grup Dizayn hazirlanmasi üçün tender elan edir',
+    feedbacks: {
+      green: 0,
+      red: 0,
+      blue: 0,
+    },
+    price: 0,
+    addedDate: 2,
+    deadLine: '11.05.2021',
     order: 0,
   },
 ];
