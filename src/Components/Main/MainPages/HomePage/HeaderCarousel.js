e88700c2cc9e8200c6aaf91a446c2adf183c9cbf
Farhad Amrah <emferhad17631@sabah.edu.az>
2021-05-01 17:37:15 +0400

    Update HeaderCarousel.js

--- src/Components/Main/MainPages/HomePage/HeaderCarousel.js
+++ src/Components/Main/MainPages/HomePage/HeaderCarousel.js
@@ -3,26 +3,26 @@ import { Carousel } from 'antd';
 import 'antd/dist/antd.css';
 import './HomePage.scss';
 
-import img1 from '../../../../assets/images/carousel_photo-1.jpg';
-import img2 from '../../../../assets/images/carousel_photo-2.jpg';
-import img3 from '../../../../assets/images/carousel_photo-3.jpg';
-import img4 from '../../../../assets/images/carousel_photo-4.jpg';
+import img1 from '../../../../assets/images/Carousel/carousel_photo-1.jpg';
+import img2 from '../../../../assets/images/Carousel/carousel_photo-2.jpg';
+import img3 from '../../../../assets/images/Carousel/carousel_photo-3.jpg';
+import img4 from '../../../../assets/images/Carousel/carousel_photo-4.jpg';
 
 const HeaderCarousel = () => {
   return (
     <>
-      <Carousel autoplay autoplaySpeed="20" dotPosition="top">
+      <Carousel autoplay autoplaySpeed='20' dotPosition='top'>
         <div>
-          <img src={img1} alt="" />
+          <img src={img1} alt='' />
         </div>
         <div>
-          <img src={img2} alt="" />
+          <img src={img2} alt='' />
         </div>
         <div>
-          <img src={img3} alt="" />
+          <img src={img3} alt='' />
         </div>
         <div>
-          <img src={img4} alt="" />
+          <img src={img4} alt='' />
         </div>
       </Carousel>
     </>
