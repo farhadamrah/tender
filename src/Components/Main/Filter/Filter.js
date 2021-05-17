70fb68ef3676c25e4224ac5af6204c6806d99f3f
Farhad Amrah <emferhad17631@sabah.edu.az>
2021-05-17 17:33:21 +0400

    version 1

--- src/Components/Main/Filter/Filter.js
+++ src/Components/Main/Filter/Filter.js
@@ -3,35 +3,46 @@ import DropDown from './DropDown/DropDown';
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Filter.scss';
+import { Collapse } from 'antd';
 
 const Filter = () => {
   const [clickFilter, setClickFilter] = useState(false);
 
-  const handleFilter = () => {
-    setClickFilter(!clickFilter);
-  };
+  const { Panel } = Collapse;
+
+  // const handleFilter = () => {
+  //   setClickFilter(!clickFilter);
+  // };
   return (
     <>
-      <section className='filter-section'>
-        <div className='filter-section--header' onClick={handleFilter}>
+      {/* <section className='filter-section'> */}
+      {/* <div className='filter-section--header' onClick={handleFilter}>
           <span className='header__title'>{'Filter'}</span>
           <div className='header__icon'>
             <FontAwesomeIcon icon={faFilter} />
           </div>
-        </div>
-        <div
+        </div> */}
+
+      <Collapse accordion>
+        <FontAwesomeIcon icon={faFilter} />
+        <Panel header={`Filter`} key='1'>
+          <DropDown />
+          <DropDown />
+          <DropDown />
+          <DropDown />
+        </Panel>
+      </Collapse>
+
+      {/* <div
           className={
             !clickFilter
               ? 'filter-section__filters'
               : 'filter-section__filters active'
           }
         >
-          <DropDown />
-          <DropDown />
-          <DropDown />
-          <DropDown />
-        </div>
-      </section>
+          
+        </div> */}
+      {/* </section> */}
     </>
   );
 };
