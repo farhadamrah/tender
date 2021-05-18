d946b5657bc5a9d9ffbb4b46c222f2c348462a56
Farhad Amrah <emferhad17631@sabah.edu.az>
2021-05-18 15:55:36 +0400

    version 1

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -34,7 +34,7 @@ const Main = () => {
             <Route path='/store' component={Store} />
             <Route path='/blog' component={Blog} />
             <Route path='/instruction' component={Instruction} />
-            <Route path='/contact-us' component={Contact} />
+            <Route path='/connection' component={Contact} />
             <Route path='/' component={HomePage} />
           </Switch>
         </section>
