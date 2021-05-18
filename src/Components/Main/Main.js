bd1ea5c25094fe95763d086e97e9b2c435fb0375
Farhad Amrah <emferhad17631@sabah.edu.az>
2021-05-18 16:34:42 +0400

    version 1

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -13,9 +13,10 @@ import HomePage from './MainPages/HomePage/HomePage';
 import Blog from './MainPages/Blog/Blog';
 import Instruction from './MainPages/Instruction/Instruction';
 import Profile from './MainPages/Profile/Profile';
-import Contact from './MainPages/FooterPages/Contact/Contact';
-import Advertisement from './MainPages/FooterPages/Advertisement/Advertisement';
+// import Contact from './MainPages/FooterPages/Contact/Contact';
+// import Advertisement from './MainPages/FooterPages/Advertisement/Contact';
 import './Main.scss';
+import Contact from './MainPages/FooterPages/Advertisement/Contact';
 
 const Main = () => {
   return (
@@ -36,7 +37,6 @@ const Main = () => {
             <Route path='/blog' component={Blog} />
             <Route path='/instruction' component={Instruction} />
             <Route path='/contact' component={Contact} />
-            <Route path='/advertisement' component={Advertisement} />
             <Route path='/' component={HomePage} />
           </Switch>
         </section>
---
+++ src/Components/Main/MainPages/FooterPages/Advertisement/Contact.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './Contact.scss';
+import Form from 'antd/lib/form/Form';
+import { Input, Button } from 'antd';
+
+const Contact = () => {
+  const message = 'Zəhmət olmazsa, məlumatı daxil edin';
+
+  return (
+    <>
+      <div className='contact container'>
+        <h1 className='contact-title'>
+          Təklif və tənqidlərinizi bizə bildirə bilərsiniz
+        </h1>
+        <Form
+          name='Əlaqə'
+          //   initialValues={{
+          //     remember: true,
+          //   }}
+          //   onFinish={onFinish}
+          //   onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            name='username'
+            rules={[
+              {
+                required: true,
+                message,
+              },
+            ]}
+          >
+            <Input placeholder='Ad,Soyad' />
+          </Form.Item>
+          <Form.Item
+            label=''
+            name='email'
+            rules={[
+              {
+                required: true,
+                message,
+              },
+            ]}
+          >
+            <Input placeholder='E-mail' />
+          </Form.Item>
+          <Form.Item
+            name='message'
+            rules={[
+              {
+                required: true,
+                message,
+              },
+            ]}
+          >
+            <Input.TextArea placeholder='Mesajınız' />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type='primary' htmlType='submit' className='send-btn'>
+              Göndər
+            </Button>
+          </Form.Item>
+          <Form.Item>
+            <Button type='primary' htmlType='submit' className='cancel-btn'>
+              Ləğv et
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
